eec1b928b1afde71f67f6e0dc499354ff7e50007
atfotiad <thfotiad@gmail.com>
2025-04-20 01:03:32 +0300

    Add PokemonExplorerApplication

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/PokemonExplorerApplication.kt
@@ -0,0 +1,8 @@
+package com.atfotiad.pokemonexplorerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokemonExplorerApplication: Application() {
+}

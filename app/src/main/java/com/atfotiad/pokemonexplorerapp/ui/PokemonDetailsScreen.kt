2d7aa60e68b6dac9dae306e378ff80aea355048d
atfotiad <thfotiad@gmail.com>
2025-04-20 17:18:39 +0300

    Add PokemonDetailsScreen

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonDetailsScreen.kt
@@ -0,0 +1,14 @@
+package com.atfotiad.pokemonexplorerapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.atfotiad.pokemonexplorerapp.model.Pokemon
+
+@Composable
+fun PokemonDetailsScreen(
+    modifier: Modifier = Modifier,
+    viewModel: PokemonDetailsViewModel,
+    pokemon: Pokemon
+) {
+
+}

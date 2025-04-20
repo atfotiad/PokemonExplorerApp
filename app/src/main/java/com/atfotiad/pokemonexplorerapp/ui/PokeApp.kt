f0cd786393a9fd28cb13fb464a203cf5e56bbee1
atfotiad <thfotiad@gmail.com>
2025-04-20 17:19:18 +0300

    Add PokeApp top-level composable

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokeApp.kt
@@ -0,0 +1,21 @@
+package com.atfotiad.pokemonexplorerapp.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PokeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val navController = rememberNavController()
+        val pokemonViewModel: PokemonViewModel = hiltViewModel()
+        PokeNavHost(navController, pokemonViewModel)
+    }
+}

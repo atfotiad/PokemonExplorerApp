9ef834ef8f844663faf519363df3b78e19353cbb
atfotiad <thfotiad@gmail.com>
2025-04-21 17:05:35 +0300

    Update PokemonItem

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonItem.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonItem.kt
@@ -1,14 +1,58 @@
 package com.atfotiad.pokemonexplorerapp.ui
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.atfotiad.pokemonexplorerapp.model.Pokemon
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun PokemonItem(
     modifier: Modifier = Modifier,
     pokemon: Pokemon,
     onPokemonClick: (Pokemon) -> Unit
 ) {
-
+    Card(
+        modifier
+            .padding(8.dp)
+            .clickable { onPokemonClick(pokemon) },
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(),
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Row(modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+            GlideImage(
+                model = pokemon.imageUrl,
+                contentDescription = pokemon.name,
+                modifier = modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colorScheme.surface, CircleShape)
+                    .padding(8.dp)
+            )
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                text = pokemon.name,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }

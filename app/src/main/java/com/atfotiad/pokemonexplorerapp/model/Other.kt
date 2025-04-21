fedf5671f8967b85b66bffe060f739c2976d34bf
atfotiad <thfotiad@gmail.com>
2025-04-21 12:13:31 +0300

    Add serializedName for official-artwork image

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/model/Other.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/model/Other.kt
@@ -1,5 +1,8 @@
 package com.atfotiad.pokemonexplorerapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Other(
+    @SerializedName("official-artwork")
     val officialArtwork: OfficialArtwork
 )

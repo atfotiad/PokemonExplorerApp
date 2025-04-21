64587364389c2a192cd36cbd2bd9342a00881144
atfotiad <thfotiad@gmail.com>
2025-04-21 10:31:03 +0300

    Add PokeMonItem composable

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonItem.kt
@@ -0,0 +1,14 @@
+package com.atfotiad.pokemonexplorerapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.atfotiad.pokemonexplorerapp.model.Pokemon
+
+@Composable
+fun PokemonItem(
+    modifier: Modifier = Modifier,
+    pokemon: Pokemon,
+    onPokemonClick: (Pokemon) -> Unit
+) {
+
+}

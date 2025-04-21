82092525b94587164ea1f106560950ece4007321
atfotiad <thfotiad@gmail.com>
2025-04-21 16:47:44 +0300

    Update PokemonViewModel to getPokeResults from api

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonViewModel.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonViewModel.kt
@@ -1,15 +1,21 @@
 package com.atfotiad.pokemonexplorerapp.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.atfotiad.pokemonexplorerapp.PokeRepository
 import com.atfotiad.pokemonexplorerapp.model.Pokemon
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 @HiltViewModel
-class PokemonViewModel : ViewModel(){
+class PokemonViewModel @Inject constructor(
+    private val repository: PokeRepository
+) : ViewModel() {
 
-    private val _pokemonList = MutableStateFlow<List<Pokemon>>(emptyList())
-    val pokemonList: StateFlow<List<Pokemon>> = _pokemonList
+    val pokemonList: Flow<PagingData<Pokemon>> =
+        repository.getPokeResults().cachedIn(viewModelScope)
 
 }

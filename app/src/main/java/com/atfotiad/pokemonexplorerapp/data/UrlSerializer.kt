c8b9287969d35d9e760364e91c79cc16ad2602f6
atfotiad <thfotiad@gmail.com>
2025-04-22 12:39:20 +0300

    Add UrlSerializer helper singleton

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/data/UrlSerializer.kt
@@ -0,0 +1,27 @@
+package com.atfotiad.pokemonexplorerapp.data
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+object UrlSerializer : KSerializer<String> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("UrlEncodedString", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): String {
+        return URLDecoder.decode(decoder.decodeString(), StandardCharsets.UTF_8.toString())
+    }
+
+    override fun serialize(encoder: Encoder, value: String) {
+        val encodedString = URLEncoder.encode(value, StandardCharsets.UTF_8.toString())
+        encoder.encodeString(encodedString)
+    }
+
+}
+

d086da5ecaffe04e92c09608a49a344645dd19f2
atfotiad <thfotiad@gmail.com>
2025-04-24 18:23:34 +0300

    Remove scaffold and handle windowInsets properly

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokeApp.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokeApp.kt
@@ -1,14 +1,16 @@
 package com.atfotiad.pokemonexplorerapp.ui
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.atfotiad.pokemonexplorerapp.navigation.PokeNavHost
 
 @Composable
 fun PokeApp() {
@@ -18,9 +20,12 @@ fun PokeApp() {
     ) {
         val navController = rememberNavController()
         val pokemonViewModel: PokemonViewModel = hiltViewModel()
-        Scaffold {
-            innerPadding ->
-            PokeNavHost(navController, pokemonViewModel, Modifier.padding(innerPadding))
-        }
+        PokeNavHost(
+            navController,
+            pokemonViewModel,
+            Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.systemBars)
+        )
     }
 }

1507ecc5e1b56ccff450889b87d1dfc9982fe868
atfotiad <thfotiad@gmail.com>
2025-04-24 18:37:53 +0300

    Improve LazyColumn with items(list) function

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonList.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonList.kt
@@ -1,24 +1,21 @@
 package com.atfotiad.pokemonexplorerapp.ui
 
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.paging.compose.LazyPagingItems
 import com.atfotiad.pokemonexplorerapp.model.Pokemon
 
 @Composable
 fun PokemonList(
     modifier: Modifier = Modifier,
-    list: LazyPagingItems<Pokemon>,
+    list: List<Pokemon>,
     onClick: (Pokemon) -> Unit
 ) {
-    LazyColumn(modifier) {
-        items(list.itemCount) { index ->
-            val pokemon = list[index]
-            if (pokemon != null) {
-                PokemonItem(modifier,pokemon) {
-                    onClick(pokemon)
-                }
+    LazyColumn(modifier = modifier) {
+        items(list) { pokemon ->
+            PokemonItem(pokemon = pokemon) {
+                onClick(pokemon)
             }
         }
     }

b2b5799292f1576d47d20515a0d0945e02225af1
atfotiad <thfotiad@gmail.com>
2025-04-24 18:44:37 +0300

    Add PokemonStatsSection composable

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/PokemonStatsSection.kt
@@ -0,0 +1,21 @@
+package com.atfotiad.pokemonexplorerapp.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.atfotiad.pokemonexplorerapp.model.Stat
+
+@Composable
+fun PokemonStatsSection(modifier: Modifier = Modifier, stats: List<Stat>) {
+    Column(
+        modifier.padding(top = 8.dp, start = 8.dp, end = 8.dp)
+    ) {
+        stats.forEach {
+            PokemonStatBar(statValue = it)
+            Spacer(modifier.padding(top = 16.dp))
+        }
+    }
+}

94f36c5f8ec575a3baab56d411b4ad290410a947
atfotiad <thfotiad@gmail.com>
2025-04-24 18:27:46 +0300

    Refactor getPokemonList logic to fetch concurrently.

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/PokeRepository.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/PokeRepository.kt
@@ -1,23 +1,61 @@
 package com.atfotiad.pokemonexplorerapp
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import com.atfotiad.pokemonexplorerapp.api.PokeClient
-import com.atfotiad.pokemonexplorerapp.paging.PokeDataSource
-import com.atfotiad.pokemonexplorerapp.paging.loadSize
+import com.atfotiad.pokemonexplorerapp.model.Pokemon
 import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 @ActivityRetainedScoped
 class PokeRepository @Inject constructor(
     private val pokeApi: PokeClient
 ) {
-    fun getPokeResults() = Pager(
-        config = PagingConfig(
-            pageSize = loadSize,
-            initialLoadSize = loadSize,
-            enablePlaceholders = false
-        ),
-        pagingSourceFactory = { PokeDataSource(pokeApi) }
-    ).flow
+    
+    fun getPokemonList(): Flow<List<Pokemon>> = flow {
+        val response = pokeApi.getAllPokemon()
+        if (response.isSuccessful && response.body() != null) {
+            val pokemonList = mutableListOf<Pokemon>()
+            coroutineScope {
+                val pokemonDetails = response.body()!!.results.map {
+                    async {
+                        val urlParts = it.url.split("/")
+                        val id = urlParts[urlParts.size - 2].toInt()
+                        val pokemonResponse = pokeApi.getPokemonInfo(id)
+                        if (pokemonResponse.isSuccessful && pokemonResponse.body() != null) {
+                            val pokemon = pokemonResponse.body()!!
+                            val pokeDexEntryText = pokeApi.getSpeciesEntry(pokemon.id)
+                            if (pokeDexEntryText.isSuccessful && pokeDexEntryText.body() != null) {
+                                Pokemon(
+                                    pokemon.id,
+                                    pokemon.name,
+                                    pokemon.types,
+                                    pokemon.stats,
+                                    pokemon.cries.latest,
+                                    pokemon.sprites.other.officialArtwork.frontDefault,
+                                    pokeDexEntryText.body()!!.flavorTextEntries.first {
+                                        it.language.name == "en"
+                                    }.flavorText
+                                )
+                            } else {
+                                null
+                            }
+                        } else {
+                            null
+                        }
+                    }
+                }
+                pokemonDetails.forEach { deferred ->
+                    deferred.await()?.let { pokemon ->
+                        pokemonList.add(pokemon)
+                    }
+                }
+            }
+            emit(pokemonList)
+        } else {
+            emit(emptyList())
+        }
+    }
 }

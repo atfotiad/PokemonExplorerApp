9d09ffe9fcc63e7f45bdd71f3163e75c381b89ae
atfotiad <thfotiad@gmail.com>
2025-04-28 03:55:44 +0300

    Fix layout in HomeScreen

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/HomeScreen.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/HomeScreen.kt
@@ -1,28 +1,38 @@
 package com.atfotiad.pokemonexplorerapp.ui
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -42,17 +52,16 @@ import com.atfotiad.pokemonexplorerapp.ui.theme.Grass
 import com.atfotiad.pokemonexplorerapp.ui.theme.Psychic
 import com.atfotiad.pokemonexplorerapp.ui.theme.Steel
 import com.atfotiad.pokemonexplorerapp.ui.theme.Water
+import kotlinx.coroutines.FlowPreview
 
 
-@OptIn(ExperimentalLayoutApi::class)
+@OptIn(ExperimentalLayoutApi::class, FlowPreview::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     pokemonViewModel: PokemonViewModel,
     onPokemonClick: (Pokemon) -> Unit
 ) {
-
-    val state by pokemonViewModel.state.collectAsStateWithLifecycle(emptyList())
 
     val types = listOf(
         "Fire",
@@ -79,110 +88,154 @@ fun HomeScreen(
         "Steel" to Steel,
         "Fairy" to Fairy,
     )
-    val selectedFilters by pokemonViewModel.selectedFilters.collectAsStateWithLifecycle()
+
+    val displayPokemonItems by pokemonViewModel.displayPokemonItems.collectAsStateWithLifecycle()
+    val totalCount by pokemonViewModel.totalPokemonCount.collectAsStateWithLifecycle()
     val text by pokemonViewModel.searchQuery.collectAsStateWithLifecycle()
+    val selectedFilters by pokemonViewModel.selectedFilters.collectAsStateWithLifecycle()
+    val isLoading by pokemonViewModel.isLoading.collectAsStateWithLifecycle()
     val focusManager = LocalFocusManager.current
+    val listState = rememberLazyListState()
 
-    ConstraintLayout(
-        modifier = modifier
-            .fillMaxSize()
-            .safeDrawingPadding()
-    ) {
-        val (searchBox, filerRow, list) = createRefs()
+    val isAllItemsLoaded by remember { derivedStateOf { displayPokemonItems.isNotEmpty() && displayPokemonItems.size == totalCount } }
+    val shouldShowLoadMoreButton by remember { derivedStateOf { !isLoading && displayPokemonItems.size < totalCount } }
 
-        TextField(
-            value = text,
-            onValueChange = { pokemonViewModel.setSearchQuery(it) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(searchBox) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
+    var isInitialLoading by remember { mutableStateOf(true) }
+
+    LaunchedEffect(isLoading, displayPokemonItems.isNotEmpty()) {
+        if (!isLoading && displayPokemonItems.isNotEmpty()) {
+            isInitialLoading = false
+        } else if (isLoading && displayPokemonItems.isEmpty()) {
+            isInitialLoading = true
+        }
+    }
+
+    Scaffold()
+    { paddingValues ->
+        ConstraintLayout(
+            modifier = modifier
+                .fillMaxSize()
+                .safeDrawingPadding()
+                .padding(paddingValues)
+        ) {
+            val (searchBox, filterRow, list) = createRefs()
+            TextField(
+                value = text,
+                onValueChange = { pokemonViewModel.setSearchQuery(it) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(searchBox) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                placeholder = {
+                    Text(text = "Search")
                 },
-            placeholder = {
-                Text(text = "Search")
-            },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
-            },
-            trailingIcon = {
-                if (text.isNotEmpty()) {
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+                },
+                trailingIcon = {
                     IconButton(onClick = {
                         pokemonViewModel.setSearchQuery("")
+                        focusManager.clearFocus()
                     }) {
                         Icon(Icons.Filled.Close, contentDescription = "Close")
                     }
-                }
-            },
-            colors = TextFieldDefaults.colors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-            ),
-            singleLine = true,
-            maxLines = 1,
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = {
-                    focusManager.clearFocus()
-                }
-            )
-        )
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                ),
+                singleLine = true,
+                maxLines = 1,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        focusManager.clearFocus()
+                    }
+                ))
 
-        FlowRow(
-            modifier = Modifier
-                .constrainAs(filerRow) {
-                    top.linkTo(searchBox.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            for (type in types) {
-                FilterChip(
-                    selected = selectedFilters.contains(type),
-                    onClick = {
+            FlowRow(
+                modifier = Modifier
+                    .constrainAs(filterRow) {
+                        top.linkTo(searchBox.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
 
-                        val newSelectedFilters = if (selectedFilters.contains(type)) {
-                            selectedFilters.filter { it != type }.toSet()
-                        } else {
-                            selectedFilters + type
-                        }
-                        pokemonViewModel.updateFilters(newSelectedFilters)
-                    },
-                    label = {
-                        Text(text = type)
-                    },
-                    colors = FilterChipDefaults.filterChipColors(
-                        selectedContainerColor = typeColorMap[type]!!,
-                        containerColor = Color.Transparent,
-                        labelColor = typeColorMap[type]!!,
-                        selectedLabelColor = Color.White
-                    ),
-                    border = BorderStroke(1.dp, typeColorMap[type]!!),
-                    modifier = Modifier
-                        .padding(horizontal = 4.dp)
-                )
+            ) {
+                for (type in types) {
+                    FilterChip(
+                        selected = selectedFilters.contains(type),
+                        onClick = {
+                            val newSelectedFilters = if (selectedFilters.contains(type)) {
+                                selectedFilters.filter { it != type }.toSet()
+                            } else {
+                                selectedFilters + type
+                            }
+                            pokemonViewModel.updateFilters(newSelectedFilters)
+
+                        },
+                        label = {
+                            Text(text = type)
+                        },
+                        colors = FilterChipDefaults.filterChipColors(
+                            selectedContainerColor = typeColorMap[type]!!,
+                            containerColor = Color.Transparent,
+                            labelColor = typeColorMap[type]!!,
+                            selectedLabelColor = Color.White
+                        ),
+                        border = BorderStroke(1.dp, typeColorMap[type]!!),
+                        modifier = Modifier
+                            .padding(horizontal = 4.dp)
+                    )
+                }
             }
-        }
-        PokemonList(
-            list = state,
-            modifier = Modifier
-                .constrainAs(list) {
-                    top.linkTo(filerRow.bottom)
+            PokemonList(
+                list = displayPokemonItems,
+                listState = listState,
+                onClick = {
+                    onPokemonClick(it)
+                },
+                modifier = Modifier
+                    .constrainAs(list) {
+                        top.linkTo(filterRow.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        height = Dimension.fillToConstraints
+                        width = Dimension.fillToConstraints
+                    }
+                    ,
+                isLoading = isLoading,
+                isAllItemsLoaded = isAllItemsLoaded,
+                shouldShowLoadMoreButton = shouldShowLoadMoreButton,
+                onLoadMore = { pokemonViewModel.loadMore() }
+            )
+
+            AnimatedVisibility(
+                visible = isLoading && displayPokemonItems.isEmpty(),
+                modifier = Modifier.constrainAs(list) {
+                    top.linkTo(filterRow.bottom)
+                    bottom.linkTo(parent.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
                     height = Dimension.fillToConstraints
-                    width = Dimension.fillToConstraints
                 }
-                .fillMaxHeight()
-        ) {
-            onPokemonClick(it)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
         }
     }
 }

8cf11acd2dec7341235f830cad830b40699682f8
atfotiad <thfotiad@gmail.com>
2025-04-30 20:23:08 +0300

    Add utility maps for type and icons

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/utils/PokemonMaps.kt
@@ -0,0 +1,61 @@
+package com.atfotiad.pokemonexplorerapp.utils
+
+import com.atfotiad.pokemonexplorerapp.R
+import com.atfotiad.pokemonexplorerapp.ui.theme.Dark
+import com.atfotiad.pokemonexplorerapp.ui.theme.Dragon
+import com.atfotiad.pokemonexplorerapp.ui.theme.Electric
+import com.atfotiad.pokemonexplorerapp.ui.theme.Fairy
+import com.atfotiad.pokemonexplorerapp.ui.theme.Fire
+import com.atfotiad.pokemonexplorerapp.ui.theme.Ghost
+import com.atfotiad.pokemonexplorerapp.ui.theme.Grass
+import com.atfotiad.pokemonexplorerapp.ui.theme.Psychic
+import com.atfotiad.pokemonexplorerapp.ui.theme.Steel
+import com.atfotiad.pokemonexplorerapp.ui.theme.Water
+
+val typeToResourceMap = mapOf(
+    "fire" to R.drawable.fire,
+    "water" to R.drawable.water,
+    "grass" to R.drawable.grass,
+    "electric" to R.drawable.electric,
+    "poison" to R.drawable.poison,
+    "normal" to R.drawable.normal,
+    "psychic" to R.drawable.psychic,
+    "rock" to R.drawable.rock,
+    "ghost" to R.drawable.ghost,
+    "ice" to R.drawable.ice,
+    "ground" to R.drawable.ground,
+    "flying" to R.drawable.flying,
+    "fighting" to R.drawable.fighting,
+    "bug" to R.drawable.bug,
+    "steel" to R.drawable.steel,
+    "dragon" to R.drawable.dragon,
+    "dark" to R.drawable.dark,
+    "fairy" to R.drawable.fairy
+)
+
+
+val types = listOf(
+    "Fire",
+    "Water",
+    "Grass",
+    "Electric",
+    "Dragon",
+    "Psychic",
+    "Ghost",
+    "Dark",
+    "Steel",
+    "Fairy"
+)
+
+val typeColorMap = mapOf(
+    "Fire" to Fire,
+    "Water" to Water,
+    "Grass" to Grass,
+    "Electric" to Electric,
+    "Dragon" to Dragon,
+    "Psychic" to Psychic,
+    "Ghost" to Ghost,
+    "Dark" to Dark,
+    "Steel" to Steel,
+    "Fairy" to Fairy,
+)

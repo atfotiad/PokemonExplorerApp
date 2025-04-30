11f3f54ec245043f0dfd3e65cdeff46461c6c51d
atfotiad <thfotiad@gmail.com>
2025-05-01 01:53:10 +0300

    Add a Remote DataSource abstraction layer

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/data/source/remote/PokeRemoteDataSource.kt
@@ -0,0 +1,31 @@
+package com.atfotiad.pokemonexplorerapp.data.source.remote
+
+import com.atfotiad.pokemonexplorerapp.api.PokeClient
+import com.atfotiad.pokemonexplorerapp.model.BaseResponse
+import com.atfotiad.pokemonexplorerapp.model.PokeDexEntryText
+import com.atfotiad.pokemonexplorerapp.model.PokemonResponse
+import com.atfotiad.pokemonexplorerapp.utils.repository.RepoUtils.toResult
+import com.atfotiad.pokemonexplorerapp.utils.repository.Result
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class PokeRemoteDataSource @Inject constructor(
+    private val pokeApi: PokeClient
+) {
+    suspend fun getPokemonList(offset: Int, limit: Int): Result<BaseResponse> {
+        return suspend { pokeApi.getAllPokemon(offset, limit) }.toResult()
+    }
+
+    suspend fun getPokemonInfo(name: String): Result<PokemonResponse> {
+        return suspend { pokeApi.getPokemonInfoByName(name) }.toResult()
+    }
+
+    suspend fun getPokemonInfo(id: Int): Result<PokemonResponse> {
+        return suspend { pokeApi.getPokemonInfo(id) }.toResult()
+    }
+
+    suspend fun getSpeciesEntry(speciesId: Int): Result<PokeDexEntryText> {
+        return suspend { pokeApi.getSpeciesEntry(speciesId) }.toResult()
+    }
+}

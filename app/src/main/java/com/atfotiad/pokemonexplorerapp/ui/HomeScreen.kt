e76ec27460a34f1e93f15a84458012328808a5d5
atfotiad <thfotiad@gmail.com>
2025-04-30 20:36:20 +0300

    Fix the HomeScreen states

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/HomeScreen.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/ui/HomeScreen.kt
@@ -1,57 +1,56 @@
 package com.atfotiad.pokemonexplorerapp.ui
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ClearAll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.atfotiad.pokemonexplorerapp.model.Pokemon
-import com.atfotiad.pokemonexplorerapp.ui.theme.Dark
-import com.atfotiad.pokemonexplorerapp.ui.theme.Dragon
-import com.atfotiad.pokemonexplorerapp.ui.theme.Electric
-import com.atfotiad.pokemonexplorerapp.ui.theme.Fairy
-import com.atfotiad.pokemonexplorerapp.ui.theme.Fire
-import com.atfotiad.pokemonexplorerapp.ui.theme.Ghost
-import com.atfotiad.pokemonexplorerapp.ui.theme.Grass
-import com.atfotiad.pokemonexplorerapp.ui.theme.Psychic
-import com.atfotiad.pokemonexplorerapp.ui.theme.Steel
-import com.atfotiad.pokemonexplorerapp.ui.theme.Water
+import com.atfotiad.pokemonexplorerapp.utils.typeColorMap
+import com.atfotiad.pokemonexplorerapp.utils.types
 import kotlinx.coroutines.FlowPreview
 
 
@@ -62,44 +61,52 @@ fun HomeScreen(
     pokemonViewModel: PokemonViewModel,
     onPokemonClick: (Pokemon) -> Unit
 ) {
-
-    val types = listOf(
-        "Fire",
-        "Water",
-        "Grass",
-        "Electric",
-        "Dragon",
-        "Psychic",
-        "Ghost",
-        "Dark",
-        "Steel",
-        "Fairy"
-    )
-
-    val typeColorMap = mapOf(
-        "Fire" to Fire,
-        "Water" to Water,
-        "Grass" to Grass,
-        "Electric" to Electric,
-        "Dragon" to Dragon,
-        "Psychic" to Psychic,
-        "Ghost" to Ghost,
-        "Dark" to Dark,
-        "Steel" to Steel,
-        "Fairy" to Fairy,
-    )
-
-    val displayPokemonItems by pokemonViewModel.displayPokemonItems.collectAsStateWithLifecycle()
     val totalCount by pokemonViewModel.totalPokemonCount.collectAsStateWithLifecycle()
     val text by pokemonViewModel.searchQuery.collectAsStateWithLifecycle()
     val selectedFilters by pokemonViewModel.selectedFilters.collectAsStateWithLifecycle()
     val isLoading by pokemonViewModel.isLoading.collectAsStateWithLifecycle()
     val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
     val listState = rememberLazyListState()
+    val isSearchingByName by pokemonViewModel.isSearchingByName.collectAsStateWithLifecycle()
+    val stateUI by pokemonViewModel.stateUI.collectAsStateWithLifecycle()
 
-    val isAllItemsLoaded by remember { derivedStateOf { displayPokemonItems.isNotEmpty() && displayPokemonItems.size == totalCount } }
-    val shouldShowLoadMoreButton by remember { derivedStateOf { !isLoading && displayPokemonItems.size < totalCount } }
+    val displayPokemonItems = remember(stateUI) {
+        if (stateUI is StateUI.Success) {
+            (stateUI as StateUI.Success).data
+        } else {
+            emptyList()
+        }
+    }
 
+    val shouldShowLoadMoreButton by rememberUpdatedState {
+        derivedStateOf {
+            !isLoading &&
+                    displayPokemonItems.size < totalCount &&
+                    !isSearchingByName &&
+                    displayPokemonItems.size != totalCount && text.isEmpty()
+        }
+    }
+
+    val shouldShowEndOfResults by rememberUpdatedState {
+        derivedStateOf {
+            !isLoading &&
+                    displayPokemonItems.isNotEmpty() &&
+                    displayPokemonItems.size == totalCount &&
+                    !isSearchingByName && text.isEmpty()
+        }
+    }
+
+    val shouldShowNoPokemonFound by rememberUpdatedState {
+        derivedStateOf {
+            !isLoading &&
+                    displayPokemonItems.isEmpty() &&
+                    !isSearchingByName && text.isNotEmpty()
+        }
+    }
+
+    val imeBottom = WindowInsets.ime.getBottom(density = LocalDensity.current)
+    var isFocused by remember { mutableStateOf(false) }
     var isInitialLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(isLoading, displayPokemonItems.isNotEmpty()) {
@@ -110,6 +117,15 @@ fun HomeScreen(
         }
     }
 
+    //Effect for handling ime bottom minimizing and restoring focus.
+    LaunchedEffect(imeBottom) {
+        if (imeBottom == 0 && isFocused) {
+            isFocused = false
+            focusManager.moveFocus(FocusDirection.Next)
+            focusManager.clearFocus()
+        }
+    }
+
     Scaffold()
     { paddingValues ->
         ConstraintLayout(
@@ -117,47 +133,37 @@ fun HomeScreen(
                 .fillMaxSize()
                 .safeDrawingPadding()
                 .padding(paddingValues)
+                .focusable()
         ) {
             val (searchBox, filterRow, list) = createRefs()
-            TextField(
-                value = text,
-                onValueChange = { pokemonViewModel.setSearchQuery(it) },
+
+            val searchBoxWidth: Dp by animateDpAsState(
+                targetValue = if (isFocused) LocalConfiguration.current.screenWidthDp.dp - 32.dp else 300.dp,
+                animationSpec = spring()
+            )
+
+            SearchField(
+                text = text,
+                onTextChange = { pokemonViewModel.setSearchQuery(it) },
+                onTrailingIconClick = {
+                    pokemonViewModel.setSearchQuery("")
+                    pokemonViewModel.performSearch()
+                    focusManager.clearFocus()
+                },
+                onSearch = {
+                    pokemonViewModel.performSearch()
+                    focusManager.clearFocus()
+                },
+                onFocusChanged = { isFocused = it.isFocused },
                 modifier = Modifier
-                    .fillMaxWidth()
                     .constrainAs(searchBox) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    },
-                placeholder = {
-                    Text(text = "Search")
-                },
-                leadingIcon = {
-                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        pokemonViewModel.setSearchQuery("")
-                        focusManager.clearFocus()
-                    }) {
-                        Icon(Icons.Filled.Close, contentDescription = "Close")
+                        top.linkTo(parent.top, margin = 8.dp)
+                        start.linkTo(parent.start, margin = 8.dp)
+                        end.linkTo(parent.end, margin = 8.dp)
+                        width = Dimension.value(searchBoxWidth)
                     }
-                },
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent,
-                ),
-                singleLine = true,
-                maxLines = 1,
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Search
-                ),
-                keyboardActions = KeyboardActions(
-                    onSearch = {
-                        focusManager.clearFocus()
-                    }
-                ))
+                    .focusRequester(focusRequester)
+            )
 
             FlowRow(
                 modifier = Modifier
@@ -170,6 +176,19 @@ fun HomeScreen(
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
 
             ) {
+                if (selectedFilters.isNotEmpty()) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        modifier = Modifier.padding(end = 8.dp)
+                    ) {
+                        Text("Filters:", style = MaterialTheme.typography.bodySmall)
+                        IconButton(onClick = pokemonViewModel::clearAllFilters) {
+                            Icon(Icons.Default.ClearAll, contentDescription = "Clear All Filters")
+                        }
+                    }
+                }
+
                 for (type in types) {
                     FilterChip(
                         selected = selectedFilters.contains(type),
@@ -198,11 +217,6 @@ fun HomeScreen(
                 }
             }
             PokemonList(
-                list = displayPokemonItems,
-                listState = listState,
-                onClick = {
-                    onPokemonClick(it)
-                },
                 modifier = Modifier
                     .constrainAs(list) {
                         top.linkTo(filterRow.bottom)
@@ -211,12 +225,17 @@ fun HomeScreen(
                         bottom.linkTo(parent.bottom)
                         height = Dimension.fillToConstraints
                         width = Dimension.fillToConstraints
-                    }
-                    ,
+                    },
+                list = displayPokemonItems,
+                listState = listState,
+                onClick = {
+                    onPokemonClick(it)
+                },
                 isLoading = isLoading,
-                isAllItemsLoaded = isAllItemsLoaded,
-                shouldShowLoadMoreButton = shouldShowLoadMoreButton,
-                onLoadMore = { pokemonViewModel.loadMore() }
+                onLoadMore = { pokemonViewModel.loadMore() },
+                shouldShowLoadMoreButton = shouldShowLoadMoreButton(),
+                shouldShowEndOfResults = shouldShowEndOfResults(),
+                shouldShowNoPokemonFound = shouldShowNoPokemonFound()
             )
 
             AnimatedVisibility(

47be05ce433ddfc1eb1a05930371f35e08b67b83
atfotiad <thfotiad@gmail.com>
2025-05-02 02:36:09 +0300

    Update PokeClient to throw exceptions

--- app/src/main/java/com/atfotiad/pokemonexplorerapp/api/PokeClient.kt
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/api/PokeClient.kt
@@ -7,10 +7,13 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.io.IOException
+import kotlin.jvm.Throws
 
 interface PokeClient {
 
     @GET("pokemon")
+    @Throws(IOException::class)
     suspend fun getAllPokemon(
         @Query("offset")
         offset: Int = 0,
@@ -20,16 +23,17 @@ interface PokeClient {
 
 
     @GET("pokemon/{id}")
-    suspend fun getPokemonInfo(
-        @Path("id") id: Int
-    ): Response<PokemonResponse>
+    @Throws(IOException::class)
+    suspend fun getPokemonInfo(@Path("id") id: Int): Response<PokemonResponse>
 
     @GET("pokemon/{name}")
+    @Throws(IOException::class)
     suspend fun getPokemonInfoByName(
         @Path("name") name: String
     ): Response<PokemonResponse>
 
     @GET("pokemon-species/{id}")
+    @Throws(Exception::class)
     suspend fun getSpeciesEntry(
         @Path("id") id: Int
     ): Response<PokeDexEntryText>

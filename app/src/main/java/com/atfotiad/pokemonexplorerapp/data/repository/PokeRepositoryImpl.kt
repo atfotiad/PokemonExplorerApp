c2d9a52f27f237f4fb412621172173328e60c45f
atfotiad <thfotiad@gmail.com>
2025-05-02 02:39:30 +0300

    Prepare PokeRepository for testing

---
+++ app/src/main/java/com/atfotiad/pokemonexplorerapp/data/repository/PokeRepositoryImpl.kt
@@ -0,0 +1,94 @@
+package com.atfotiad.pokemonexplorerapp.data.repository
+
+import com.atfotiad.pokemonexplorerapp.data.model.Pokemon
+import com.atfotiad.pokemonexplorerapp.data.model.PokemonResponse
+import com.atfotiad.pokemonexplorerapp.data.source.remote.PokeRemoteDataSource
+import com.atfotiad.pokemonexplorerapp.utils.repository.Result
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@ActivityRetainedScoped
+@Suppress("UNCHECKED_CAST")
+class PokeRepositoryImpl @Inject constructor(
+    private val remoteDataSource: PokeRemoteDataSource
+): PokeRepository {
+    private val _totalPokemonCount = MutableStateFlow(0)
+    override val totalPokemonCount = _totalPokemonCount.asStateFlow()
+
+
+    override fun getPokemonByName(name: String): Flow<Result<Pokemon>> = flow {
+        emit(Result.Loading)
+        val pokemonInfoResult = remoteDataSource.getPokemonInfo(name)
+        if (pokemonInfoResult is Result.Success) {
+            emit(fetchSpeciesAndCreatePokemon(pokemonInfoResult.data))
+        } else {
+            emit(pokemonInfoResult as Result<Pokemon>)
+        }
+    }
+
+    override fun getPokemonList(offset: Int, limit: Int): Flow<Result<List<Pokemon>>> = flow {
+        emit(Result.Loading)
+        val pokemonListResponseResult = remoteDataSource.getPokemonList(offset, limit)
+        if (pokemonListResponseResult is Result.Success) {
+            pokemonListResponseResult.data?.let { response ->
+                _totalPokemonCount.value = response.count
+                val pokemonList = mutableListOf<Pokemon>()
+                coroutineScope {
+                    val pokemonDetailsDeferred = response.results.map { basicPokemon ->
+                        async {
+                            val id = basicPokemon.url.split("/").dropLast(1).last().toInt()
+                            val pokemonInfoResult = remoteDataSource.getPokemonInfo(id)
+                            if (pokemonInfoResult is Result.Success) {
+                                fetchSpeciesAndCreatePokemon(pokemonInfoResult.data)
+                            } else {
+                                null
+                            }
+                        }
+                    }
+                    pokemonDetailsDeferred.forEach { deferred ->
+                        (deferred.await() as? Result.Success)?.data?.let { pokemonList.add(it) }
+                    }
+                }
+                emit(Result.Success(pokemonList))
+            } ?: emit(Result.Success(emptyList()))
+        } else {
+            emit(pokemonListResponseResult as Result<List<Pokemon>>)
+        }
+    }
+
+    private suspend fun fetchSpeciesAndCreatePokemon(pokemonResponse: PokemonResponse?): Result<Pokemon> {
+        return pokemonResponse?.let {
+            val speciesId = it.species.url.split("/").dropLast(1).last().toInt()
+            println("Extracted speciesId: $speciesId") // Debug log
+            val speciesEntryResult = remoteDataSource.getSpeciesEntry(speciesId)
+
+            return if (speciesEntryResult is Result.Success) {
+                val flavorText = speciesEntryResult.data?.flavorTextEntries?.firstOrNull { entry -> entry.language.name == "en" }?.flavorText ?: ""
+                val imageUrl = it.sprites.other.officialArtwork.frontDefault
+                    ?: it.sprites.other.home.frontDefault
+                    ?: "https://freesvg.org/img/Simple-Image-Not-Found-Icon.png"
+                Result.Success(
+                    Pokemon(
+                        it.id,
+                        it.name,
+                        it.types,
+                        it.stats,
+                        it.species,
+                        it.cries.latest,
+                        imageUrl,
+                        flavorText
+                    )
+                )
+            } else {
+                speciesEntryResult as Result<Pokemon>
+            }
+        } ?: Result.Success(null)
+    }
+}
